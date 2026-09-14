508de29976fd46faede5f9c948b02caa28ae7f62
harshit karnwal <karnwalharshit712@gmail.com>
2026-09-14 23:17:40 +0530

    Add day 36 code

---
+++ day-36/q71.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, cols, i, j;
+
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+
+    // Read matrix
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Print matrix
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
